ae22a8b06b64dacd4071f4290e2762141525c4d1
Rudigus <rodrigo.matos9@hotmail.com>
2021-06-21 08:51:46 -0300

    adds initial xcode project files

---
+++ Shared/ChaseNEATApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChaseNEATApp.swift
+//  Shared
+//
+//  Created by Rudigus on 21/06/21.
+//
+
+import SwiftUI
+
+@main
+struct ChaseNEATApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
